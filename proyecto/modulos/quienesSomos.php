c6b15020659816443421b073e2b3b6b9d243bf1a
DanielGeek <daniel30081990@gmail.com>
2018-09-25 10:37:41 -0400

    mejorado quienes somos

--- proyecto/modulos/quienesSomos.php
+++ proyecto/modulos/quienesSomos.php
@@ -1,17 +1,16 @@
-<section id="quienessomos" class="section section-lg text-center">
-    <div class="container">
-        <div class="row row-50">
-            <div class="col-sm-12 col-lg-12 wow-outer">
-                <article class="profile-classic wow slideInLeft">
-                <div class="profile-classic-main"><img class="profile-classic-image" src="images/logo.jpg" alt="" width="270" height="273"/>
-                    <div class="profile-classic-caption">
-                    <h6 class="profile-classic-title">En SAED prestamos un servicio integral de Capacitación, Asesoría y Aplicación de revestimientos Arquitectónicos e Industriales.</h6><br>
-                     <h6 class="profile-classic-title">Contamos con un equipo humano altamente calificado con más de 15 años de experiencia en el mercado de la pintura, agregando valor a los diversos proyectos de nuestros clientes.</h6>
-                    </div>
-                </div>
-                
-                </article>
-            </div>
-        </div>    
+
+<!-- Overview-->
+<section id="quienessomos" class="section section-lg">
+<div class="container">
+    <div class="row row-50 justify-content-center justify-content-lg-between">
+    <div class="col-md-10 col-lg-6">
+        <h3>Quienes Somos</h3>
+        <h4 class="offset-top-3">En SAED prestamos un servicio integral de Capacitación, Asesoría y Aplicación de revestimientos Arquitectónicos e Industriales.</h4>
+        <p>Contamos con un equipo humano altamente calificado con más de 15 años de experiencia en el mercado de la pintura, agregando valor a los diversos proyectos de nuestros clientes.</p>
+        <div class="group group-middle"><a class="button button-primary button-winona page-scroll" href="#contacto" data-waypoint-to="#our-history">Contactanos</a></div>
     </div>
+    <div class="col-md-10 col-lg-6 col-xl-5"> <img class="img-responsive" src="images/logo.jpg" alt="" width="470" height="334"/>
+    </div>
+    </div>
+</div>
 </section>
